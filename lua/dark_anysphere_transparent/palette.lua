5409240370f0a628d2f6cceeb65d32a244e66f82
z-f-x <benjamin.roedstien@gmail.com>
2025-05-06 15:25:16 +0200

    Fager hentet fra cursor sin json

--- lua/dark_anysphere_transparent/palette.lua
+++ lua/dark_anysphere_transparent/palette.lua
@@ -1,27 +1,51 @@
-
 local M = {}
 
--- Assume the new palette is provided by VSCodeModernPalette
-local palette = require("vscode_modern_palette")
---[[
-  VSCodeModernPalette mapping used:
+-- Anysphere colorscheme palette
+M.grey_01 = '#141414'     -- darkest background/sidebar
+M.grey_02 = '#1a1a1a'     -- editor background
+M.grey_03 = '#292929'     -- line highlight
+M.grey_04 = '#2A2A2A'     -- dropdown border
+M.grey_05 = '#404040'     -- selection background
+M.grey_06 = '#505050'     -- inactive line number
+M.grey_07 = '#CCCCCC'     -- main text/foreground
+M.grey_08 = '#D8DEE9'     -- editor foreground
+M.grey_09 = '#FFFFFF'     -- active line number
 
-  bg          -> palette.grey_01    -- (#252526)
-  fg          -> palette.light_13   -- (#ebebeb)
-  gray        -> palette.grey_14    -- (#6e7681)
-  blue        -> palette.blue_16    -- (#2aaaff)
-  light_blue  -> palette.blue_25    -- (#9cdcfe)
-  cyan        -> palette.blue_28    -- (#c3d8e6)
-  green       -> palette.green_08   -- (#4ec9b0)
-  light_green -> palette.green_13   -- (#b5cea8)
-  yellow      -> palette.yellow_08  -- (#e2c08d)
-  orange      -> palette.orange_03  -- (#f8c9ab)
-  red         -> palette.red_08     -- (#f14c4c)
-  magenta     -> palette.magenta_01 -- (#d16d9e)
-  purple      -> palette.purple_02  -- (#b180d7)
-  dark_purple -> palette.purple_01  -- (#af00db)
-  ui_blue     -> palette.blue_14    -- (#264f78)
-]]--
+-- Accent colors from Anysphere theme
+M.blue_01 = '#88C0D0'     -- activity badge background
+M.blue_02 = '#4c9df3'     -- button background
+M.blue_03 = '#66aefa'     -- button hover
+M.blue_04 = '#2472c8'     -- blue accent
+M.blue_05 = '#81A1C1'     -- terminal blue
+M.blue_06 = '#264f78'     -- selection highlight
+
+M.red_01 = '#BF616A'      -- error foreground
+M.red_02 = '#f14c4c'      -- minimap error highlight
+
+M.green_01 = '#A3BE8C'    -- git added
+M.green_02 = '#15ac91'    -- minimap added
+
+M.yellow_01 = '#EBCB8B'   -- warning foreground
+M.yellow_02 = '#e5b95c'   -- minimap modified
+M.yellow_03 = '#ea7620'   -- minimap warning
+
+-- Transparency
+M.none = 'NONE'
+M.none_1 = '#00000000'
+
+-- Semantic token colors from Anysphere theme
+M.semantic = {
+    purple = '#AA9BF5',    -- variable.other.property
+    orange = '#efb080',    -- function.declaration
+    cyan = '#82d2ce',      -- class.typeHint
+    func = '#ebc88d',      -- function
+    string = '#e394dc',    -- string
+    parameter = '#d6d6dd', -- parameter function
+    comment = '#6d6d6d',   -- comments
+    decorator = '#a8cc7c', -- python decorator
+    class = '#87c3ff',     -- class
+    selfParam = '#cc7c8a'  -- selfParameter
+}
 
 function M.setup()
   vim.cmd("hi clear")
@@ -34,140 +58,140 @@ function M.setup()
 
   local groups = {
     -- Vim interface
-    Normal = { fg = palette.light_13, bg = palette.grey_01 },
-    NormalFloat = { fg = palette.light_13, bg = palette.grey_01 },
-    Cursor = { fg = palette.grey_01, bg = palette.light_13 },
-    CursorLine = { bg = palette.grey_07 }, -- e.g. approximating "#292929"
-    CursorColumn = { bg = palette.grey_07 },
-    ColorColumn = { bg = palette.grey_07 },
-    LineNr = { fg = palette.grey_14 },
-    CursorLineNr = { fg = palette.light_13 },
-    VertSplit = { fg = palette.grey_09 },
-    Folded = { fg = palette.grey_14, bg = palette.grey_07 },
-    FoldColumn = { fg = palette.grey_14, bg = palette.grey_01 },
-    SignColumn = { bg = palette.grey_01 },
-    StatusLine = { fg = palette.light_13, bg = palette.grey_07 },
-    StatusLineNC = { fg = palette.grey_14, bg = palette.grey_07 },
-    Pmenu = { fg = palette.light_13, bg = palette.grey_07 },
-    PmenuSel = { fg = palette.light_13, bg = palette.blue_14 },
-    PmenuSbar = { bg = palette.grey_07 },
-    PmenuThumb = { bg = palette.grey_14 },
-    MatchParen = { fg = palette.yellow_08, bold = true },
-    NonText = { fg = palette.grey_07 },
-    SpecialKey = { fg = palette.grey_07 },
-    Visual = { bg = palette.blue_14 },
-    VisualNOS = { bg = palette.blue_14 },
-    Search = { fg = palette.grey_01, bg = palette.orange_02 },
-    IncSearch = { fg = palette.grey_01, bg = palette.orange_02 },
-    QuickFixLine = { bg = palette.blue_14 },
-    Terminal = { fg = palette.light_13, bg = palette.dark_02 },
+    Normal = { fg = M.grey_07, bg = M.grey_01 },
+    NormalFloat = { fg = M.grey_07, bg = M.grey_01 },
+    Cursor = { fg = M.grey_01, bg = M.grey_07 },
+    CursorLine = { bg = M.grey_07 }, -- e.g. approximating "#292929"
+    CursorColumn = { bg = M.grey_07 },
+    ColorColumn = { bg = M.grey_07 },
+    LineNr = { fg = M.grey_08 },
+    CursorLineNr = { fg = M.grey_07 },
+    VertSplit = { fg = M.grey_09 },
+    Folded = { fg = M.grey_08, bg = M.grey_07 },
+    FoldColumn = { fg = M.grey_08, bg = M.grey_01 },
+    SignColumn = { bg = M.grey_01 },
+    StatusLine = { fg = M.grey_07, bg = M.grey_07 },
+    StatusLineNC = { fg = M.grey_08, bg = M.grey_07 },
+    Pmenu = { fg = M.grey_07, bg = M.grey_07 },
+    PmenuSel = { fg = M.grey_07, bg = M.blue_04 },
+    PmenuSbar = { bg = M.grey_07 },
+    PmenuThumb = { bg = M.grey_08 },
+    MatchParen = { fg = M.yellow_01, bold = true },
+    NonText = { fg = M.grey_07 },
+    SpecialKey = { fg = M.grey_07 },
+    Visual = { bg = M.blue_04 },
+    VisualNOS = { bg = M.blue_04 },
+    Search = { fg = M.grey_01, bg = M.yellow_02 },
+    IncSearch = { fg = M.grey_01, bg = M.yellow_02 },
+    QuickFixLine = { bg = M.blue_04 },
+    Terminal = { fg = M.grey_07, bg = M.grey_02 },
 
     -- Syntax
-    Comment = { fg = palette.grey_14, italic = true },
-    Constant = { fg = palette.orange_03 },
-    String = { fg = palette.magenta_01 },
-    Character = { fg = palette.magenta_01 },
-    Number = { fg = palette.yellow_08 },
-    Boolean = { fg = palette.blue_16 },
-    Float = { fg = palette.yellow_08 },
-    Identifier = { fg = palette.blue_16 },
-    Function = { fg = palette.yellow_08 },
-    Statement = { fg = palette.blue_16 },
-    Conditional = { fg = palette.blue_16 },
-    Repeat = { fg = palette.blue_16 },
-    Label = { fg = palette.blue_16 },
-    Operator = { fg = palette.light_13 },
-    Keyword = { fg = palette.blue_16 },
-    Exception = { fg = palette.blue_16 },
-    PreProc = { fg = palette.blue_16 },
-    Include = { fg = palette.blue_16 },
-    Define = { fg = palette.blue_16 },
-    Macro = { fg = palette.blue_16 },
-    PreCondit = { fg = palette.blue_16 },
-    Type = { fg = palette.orange_03 },
-    StorageClass = { fg = palette.blue_16 },
-    Structure = { fg = palette.blue_16 },
-    Typedef = { fg = palette.blue_16 },
-    Special = { fg = palette.yellow_08 },
-    SpecialChar = { fg = palette.yellow_08 },
-    Tag = { fg = palette.orange_03 },
-    Delimiter = { fg = palette.light_13 },
-    SpecialComment = { fg = palette.grey_14 },
-    Debug = { fg = palette.orange_03 },
+    Comment = { fg = M.grey_08, italic = true },
+    Constant = { fg = M.yellow_01 },
+    String = { fg = M.purple },
+    Character = { fg = M.purple },
+    Number = { fg = M.yellow_01 },
+    Boolean = { fg = M.blue_04 },
+    Float = { fg = M.yellow_01 },
+    Identifier = { fg = M.blue_04 },
+    Function = { fg = M.yellow_01 },
+    Statement = { fg = M.blue_04 },
+    Conditional = { fg = M.blue_04 },
+    Repeat = { fg = M.blue_04 },
+    Label = { fg = M.blue_04 },
+    Operator = { fg = M.grey_07 },
+    Keyword = { fg = M.blue_04 },
+    Exception = { fg = M.blue_04 },
+    PreProc = { fg = M.blue_04 },
+    Include = { fg = M.blue_04 },
+    Define = { fg = M.blue_04 },
+    Macro = { fg = M.blue_04 },
+    PreCondit = { fg = M.blue_04 },
+    Type = { fg = M.yellow_01 },
+    StorageClass = { fg = M.blue_04 },
+    Structure = { fg = M.blue_04 },
+    Typedef = { fg = M.blue_04 },
+    Special = { fg = M.yellow_01 },
+    SpecialChar = { fg = M.yellow_01 },
+    Tag = { fg = M.yellow_01 },
+    Delimiter = { fg = M.grey_07 },
+    SpecialComment = { fg = M.grey_08 },
+    Debug = { fg = M.yellow_01 },
     Underlined = { underline = true },
-    Ignore = { fg = palette.grey_14 },
-    Error = { fg = palette.red_08 },
-    Todo = { fg = palette.magenta_01, bold = true },
+    Ignore = { fg = M.grey_08 },
+    Error = { fg = M.red_01 },
+    Todo = { fg = M.purple, bold = true },
 
     -- Treesitter
-    ["@punctuation.delimiter"] = { fg = palette.light_13 },
-    ["@punctuation.bracket"] = { fg = palette.light_13 },
-    ["@punctuation.special"] = { fg = palette.blue_16 },
-    ["@constant"] = { fg = palette.orange_03 },
-    ["@constant.builtin"] = { fg = palette.orange_03 },
-    ["@constant.macro"] = { fg = palette.orange_03 },
-    ["@string.regex"] = { fg = palette.red_08 },
-    ["@string"] = { fg = palette.magenta_01 },
-    ["@character"] = { fg = palette.magenta_01 },
-    ["@number"] = { fg = palette.yellow_08 },
-    ["@boolean"] = { fg = palette.blue_16 },
-    ["@float"] = { fg = palette.yellow_08 },
-    ["@annotation"] = { fg = palette.yellow_08 },
-    ["@attribute"] = { fg = palette.blue_16 },
-    ["@namespace"] = { fg = palette.orange_03 },
-    ["@function.builtin"] = { fg = palette.blue_16 },
-    ["@function"] = { fg = palette.yellow_08 },
-    ["@function.macro"] = { fg = palette.yellow_08 },
-    ["@parameter"] = { fg = palette.light_13 },
-    ["@parameter.reference"] = { fg = palette.light_13 },
-    ["@method"] = { fg = palette.yellow_08 },
-    ["@field"] = { fg = palette.purple_02 },
-    ["@property"] = { fg = palette.purple_02 },
-    ["@constructor"] = { fg = palette.blue_16 },
-    ["@conditional"] = { fg = palette.blue_16 },
-    ["@repeat"] = { fg = palette.blue_16 },
-    ["@label"] = { fg = palette.blue_16 },
-    ["@keyword"] = { fg = palette.blue_16 },
-    ["@keyword.function"] = { fg = palette.blue_16 },
-    ["@keyword.operator"] = { fg = palette.blue_16 },
-    ["@operator"] = { fg = palette.light_13 },
-    ["@exception"] = { fg = palette.blue_16 },
-    ["@type"] = { fg = palette.orange_03 },
-    ["@type.builtin"] = { fg = palette.orange_03 },
-    ["@structure"] = { fg = palette.orange_03 },
-    ["@include"] = { fg = palette.blue_16 },
-    ["@variable"] = { fg = palette.blue_25 },
-    ["@variable.builtin"] = { fg = palette.orange_03 },
+    ["@punctuation.delimiter"] = { fg = M.grey_07 },
+    ["@punctuation.bracket"] = { fg = M.grey_07 },
+    ["@punctuation.special"] = { fg = M.blue_04 },
+    ["@constant"] = { fg = M.yellow_01 },
+    ["@constant.builtin"] = { fg = M.yellow_01 },
+    ["@constant.macro"] = { fg = M.yellow_01 },
+    ["@string.regex"] = { fg = M.red_01 },
+    ["@string"] = { fg = M.purple },
+    ["@character"] = { fg = M.purple },
+    ["@number"] = { fg = M.yellow_01 },
+    ["@boolean"] = { fg = M.blue_04 },
+    ["@float"] = { fg = M.yellow_01 },
+    ["@annotation"] = { fg = M.yellow_01 },
+    ["@attribute"] = { fg = M.blue_04 },
+    ["@namespace"] = { fg = M.yellow_01 },
+    ["@function.builtin"] = { fg = M.blue_04 },
+    ["@function"] = { fg = M.yellow_01 },
+    ["@function.macro"] = { fg = M.yellow_01 },
+    ["@parameter"] = { fg = M.grey_07 },
+    ["@parameter.reference"] = { fg = M.grey_07 },
+    ["@method"] = { fg = M.yellow_01 },
+    ["@field"] = { fg = M.purple },
+    ["@property"] = { fg = M.purple },
+    ["@constructor"] = { fg = M.blue_04 },
+    ["@conditional"] = { fg = M.blue_04 },
+    ["@repeat"] = { fg = M.blue_04 },
+    ["@label"] = { fg = M.blue_04 },
+    ["@keyword"] = { fg = M.blue_04 },
+    ["@keyword.function"] = { fg = M.blue_04 },
+    ["@keyword.operator"] = { fg = M.blue_04 },
+    ["@operator"] = { fg = M.grey_07 },
+    ["@exception"] = { fg = M.blue_04 },
+    ["@type"] = { fg = M.yellow_01 },
+    ["@type.builtin"] = { fg = M.yellow_01 },
+    ["@structure"] = { fg = M.yellow_01 },
+    ["@include"] = { fg = M.blue_04 },
+    ["@variable"] = { fg = M.blue_05 },
+    ["@variable.builtin"] = { fg = M.yellow_01 },
 
     -- LSP
-    DiagnosticError = { fg = palette.red_08 },
-    DiagnosticWarn = { fg = palette.yellow_08 },
-    DiagnosticInfo = { fg = palette.blue_16 },
-    DiagnosticHint = { fg = palette.blue_16 },
-    LspReferenceText = { bg = palette.grey_07 },
-    LspReferenceRead = { bg = palette.grey_07 },
-    LspReferenceWrite = { bg = palette.grey_07 },
+    DiagnosticError = { fg = M.red_01 },
+    DiagnosticWarn = { fg = M.yellow_01 },
+    DiagnosticInfo = { fg = M.blue_04 },
+    DiagnosticHint = { fg = M.blue_04 },
+    LspReferenceText = { bg = M.grey_07 },
+    LspReferenceRead = { bg = M.grey_07 },
+    LspReferenceWrite = { bg = M.grey_07 },
 
     -- Plugins
-    TelescopeNormal = { fg = palette.light_13, bg = palette.grey_01 },
-    TelescopeBorder = { fg = palette.grey_09, bg = palette.grey_01 },
-    TelescopePromptNormal = { fg = palette.light_13, bg = palette.grey_07 },
-    TelescopePromptBorder = { fg = palette.grey_09, bg = palette.grey_07 },
-    TelescopePromptTitle = { fg = palette.light_13, bg = palette.blue_14 },
-    TelescopePreviewTitle = { fg = palette.light_13, bg = palette.blue_14 },
-    TelescopeResultsTitle = { fg = palette.light_13, bg = palette.blue_14 },
+    TelescopeNormal = { fg = M.grey_07, bg = M.grey_01 },
+    TelescopeBorder = { fg = M.grey_09, bg = M.grey_01 },
+    TelescopePromptNormal = { fg = M.grey_07, bg = M.grey_07 },
+    TelescopePromptBorder = { fg = M.grey_09, bg = M.grey_07 },
+    TelescopePromptTitle = { fg = M.grey_07, bg = M.blue_04 },
+    TelescopePreviewTitle = { fg = M.grey_07, bg = M.blue_04 },
+    TelescopeResultsTitle = { fg = M.grey_07, bg = M.blue_04 },
 
-    GitSignsAdd = { fg = palette.green_08 },
-    GitSignsChange = { fg = palette.blue_16 },
-    GitSignsDelete = { fg = palette.red_08 },
+    GitSignsAdd = { fg = M.green_01 },
+    GitSignsChange = { fg = M.blue_04 },
+    GitSignsDelete = { fg = M.red_01 },
 
-    BufferLineFill = { bg = palette.grey_07 },
-    BufferLineBackground = { fg = palette.grey_14, bg = palette.grey_07 },
-    BufferLineBufferVisible = { fg = palette.light_13, bg = palette.grey_07 },
-    BufferLineBufferSelected = { fg = palette.light_13, bg = palette.grey_01, bold = true },
+    BufferLineFill = { bg = M.grey_07 },
+    BufferLineBackground = { fg = M.grey_08, bg = M.grey_07 },
+    BufferLineBufferVisible = { fg = M.grey_07, bg = M.grey_07 },
+    BufferLineBufferSelected = { fg = M.grey_07, bg = M.grey_01, bold = true },
 
-    IndentBlanklineChar = { fg = palette.grey_07 },
-    IndentBlanklineContextChar = { fg = palette.purple_02 },
+    IndentBlanklineChar = { fg = M.grey_07 },
+    IndentBlanklineContextChar = { fg = M.purple },
   }
 
   for group, styles in pairs(groups) do
